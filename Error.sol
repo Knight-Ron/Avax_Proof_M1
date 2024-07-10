8b4fa4645f4ef5340cc47f072583b532821b0cdd
Knight-Ron <ronaldjacobofficial@gmail.com>
2024-07-10 21:29:19 +0530

    Update Error.sol

--- Error.sol
+++ Error.sol
@@ -1,149 +1,41 @@
-import { useState, useEffect } from "react";
-import { ethers } from "ethers";
-import atm_abi from "../artifacts/contracts/Assessment.sol/Assessment.json";
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.26;
 
-export default function HomePage() {
-  const [ethWallet, setEthWallet] = useState(undefined);
-  const [account, setAccount] = useState(undefined);
-  const [atm, setATM] = useState(undefined);
-  const [balance, setBalance] = useState(undefined);
-
-  const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
-  const atmABI = atm_abi.abi;
-
-  const getWallet = async () => {
-    if (window.ethereum) {
-      setEthWallet(window.ethereum);
+contract Error {
+    function testRequire(uint _input) public pure {
+        // Require should be used to validate conditions such as:
+        // - inputs
+        // - conditions before execution
+        // - return values from calls to other functions
+        require(_input > 10, "Input must be greater than 10");
     }
 
-    if (ethWallet) {
-      const account = await ethWallet.request({ method: "eth_accounts" });
-      handleAccount(account);
-    }
-  };
-
-  const handleAccount = (account) => {
-    if (account) {
-      console.log("Account connected: ", account);
-      setAccount(account);
-    } else {
-      console.log("No account found");
-    }
-  };
-
-  const connectAccount = async () => {
-    if (!ethWallet) {
-      alert("MetaMask wallet is required to connect");
-      return;
-    }
-
-    const accounts = await ethWallet.request({ method: "eth_requestAccounts" });
-    handleAccount(accounts);
-
-    // once wallet is set we can get a reference to our deployed contract
-    getATMContract();
-  };
-
-  const getATMContract = () => {
-    const provider = new ethers.providers.Web3Provider(ethWallet);
-    const signer = provider.getSigner();
-    const atmContract = new ethers.Contract(contractAddress, atmABI, signer);
-
-    setATM(atmContract);
-  };
-
-  const getBalance = async () => {
-    if (atm) {
-      setBalance((await atm.getBalance()).toNumber());
-    }
-  };
-
-  const deposit = async () => {
-    if (atm) {
-      let tx = await atm.deposit(1);
-      await tx.wait();
-      getBalance();
-    }
-  };
-
-  const withdraw = async () => {
-    if (atm) {
-      let tx = await atm.withdraw(1);
-      await tx.wait();
-      getBalance();
-    }
-  };
-
-  const initUser = () => {
-    // Check to see if user has Metamask
-    if (!ethWallet) {
-      return <p>Please install Metamask in order to use this ATM.</p>;
-    }
-
-    // Check to see if user is connected. If not, connect to their account
-    if (!account) {
-      return <button onClick={connectAccount}>Please connect your Metamask wallet</button>;
-    }
-
-    if (balance == undefined) {
-      getBalance();
-    }
-
-    return (
-      <div>
-        <p style={{ color: 'white' }}>Your Account: {account}</p>
-        <p style={{ color: 'white' }}>Your Balance: {balance}</p>
-        <button onClick={deposit}>Deposit 1 ETH</button>
-        <button onClick={withdraw}>Withdraw 1 ETH</button>
-      </div>
-    );
-  };
-
-  useEffect(() => {
-    getWallet();
-  }, []);
-
-  return (
-    <main className="container">
-      <header><h1>Welcome to the Metacrafters ATM!</h1></header>
-      {initUser()}
-      <style jsx>{`
-        @font-face {
-          font-family: 'Harlow Solid Italic';
-          src: url('/fonts/HarlowSolidItalic.ttf') format('truetype');
+    function testRevert(uint _input) public pure {
+        // Revert is useful when the condition to check is complex.
+        // This code does the exact same thing as the example above
+        if (_input <= 10) {
+            revert("Input must be greater than 10");
         }
-        .container {
-          display: flex;
-          flex-direction: column;
-          align-items: center;
-          justify-content: center;
-          height: 100vh;
-          background: linear-gradient(120deg, #0f0c29, #302b63, #24243e);
-          padding: 50px;
-          font-family: 'Harlow Solid Italic', cursive;
-          color: #fff;
+    }
+
+    uint public number;
+
+    function testAssert() public view {
+        // Assert should only be used to test for internal errors,
+        // and to check invariants.
+
+        // Here we assert that number is always equal to 0
+        // since it is impossible to update the value of number
+        assert(number == 0);
+    }
+
+    // custom error
+    error InsufficientBalance(uint balance, uint withdrawAmount);
+
+    function testCustomError(uint _NewWithdrawAmount) public view {
+        uint NewBalance = address(this).balance;
+        if (NewBalance < _NewWithdrawAmount) {
+            revert InsufficientBalance({balance: NewBalance, withdrawAmount: _NewWithdrawAmount});
         }
-        header h1 {
-          font-size: 3em;
-          color: #fff;
-          margin-bottom: 20px;
-        }
-        p {
-          font-size: 1.5em;
-        }
-        button {
-          font-size: 1.5em;
-          padding: 10px 20px;
-          margin: 10px;
-          background-color: #333;
-          color: #fff;
-          border: none;
-          cursor: pointer;
-        }
-        button:hover {
-          background-color: #555;
-        }
-      `}</style>
-    </main>
-  );
+    }
 }
